70b4aaa8c908a7d9ee1269c74f2b34349bcdc179
mioe <mioe.boss@gmail.com>
2022-03-24 22:00:29 +0300

    l036

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var questIdx = 0;
+
+  void handleClickAnswer() {
+    setState(() {
+      questIdx += 1;
+    });
+    print(questIdx);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('hello'));
+    var questions = [
+      'What\'s yout favorite color?',
+      'What\'s yout favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[questIdx],
+            ),
+            RaisedButton(
+              onPressed: handleClickAnswer,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: handleClickAnswer,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: handleClickAnswer,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
