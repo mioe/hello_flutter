7a3490e2b05205a32b8de2ee2d99184d35c1054b
mioe <mioe.boss@gmail.com>
2022-04-21 21:28:44 +0300

    end hello_flutter app

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -18,11 +18,57 @@ class Quiz extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        RaisedButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Text('(deprecated) RaisedButton'),
+          onPressed: () => {},
+        ),
+        ElevatedButton(
+          // style: ButtonStyle(
+          //   backgroundColor: MaterialStateProperty.all(Colors.red),
+          //   foregroundColor: MaterialStateProperty.all(Colors.white),
+          // ),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.orange,
+            onPrimary: Colors.black,
+          ),
+          child: Text('RaisedButton -> ElevatedButton'),
+          onPressed: () => {},
+        ),
+        FlatButton(
+          textColor: Colors.blue,
+          child: Text('(deprecated) FlatButton'),
+          onPressed: () => {},
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            primary: Colors.orange,
+          ),
+          child: Text('FlatButton -> TextButton'),
+          onPressed: () => {},
+        ),
+        OutlineButton(
+          borderSide: BorderSide(color: Colors.blue),
+          textColor: Colors.blue,
+          child: Text('(deprecated) OutlineButton'),
+          onPressed: () => {},
+        ),
+        OutlinedButton(
+          child: Text('OutlineButton -> OutlinedButton'),
+          style: TextButton.styleFrom(
+            primary: Colors.orange,
+            side: BorderSide(color: Colors.red),
+          ),
+          onPressed: () => {},
+        ),
         Question(
           questions[questIdx]['text'] as String,
         ),
-        ...(questions[questIdx]['answers'] as List<Map<String, Object>>).map((answer) {
-          return Answer(answer['text'] as String, () => handleClickAnswer(answer['score']));
+        ...(questions[questIdx]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(answer['text'] as String,
+              () => handleClickAnswer(answer['score']));
         }).toList()
       ],
     );
